d78159ec227259ec77f13167347174e64a3fe058
Silveira <Luan.DaSilveira@thomsonreuters.com>
2018-07-03 11:41:27 -0300

    03/07/2018

--- app/Model/Admin/Paciente.php
+++ app/Model/Admin/Paciente.php
@@ -4,10 +4,14 @@ namespace App\Model\Admin;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Model\Admin\Ocorrencia;
+use Mpociot\Firebase\SyncsWithFirebase;
 
 class Paciente extends Model{
+    use SyncsWithFirebase;
+
     protected $table = 'pacientes';
-    protected $guarded = ['id'];    
+    protected $guarded = ['id'];
+    protected $fillable = ['nome','data_nascimento','tipo_sanguineo','fator_rh_sanguineo','endereco','informacoes_medicas'];
 
     public function ocorrencias(){
         return $this->hasMany(Ocorrencia::class, 'id');

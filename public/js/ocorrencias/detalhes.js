9005f80db6c36b6fba5e0bb8d9a47f32757fac69
Silveira <Luan.DaSilveira@thomsonreuters.com>
2018-07-04 14:52:58 -0300

    AJustado a página de ocorrências

--- public/js/ocorrencias/detalhes.js
+++ public/js/ocorrencias/detalhes.js
@@ -5,5 +5,5 @@ function initMap() {
   var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 4, center: position});
 
-  var marker = new google.maps.Marker({position: position, map: map});
+  var marker = new google.maps.Marker({position: position, map: map, draggable: true});
 }
